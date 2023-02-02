69c15dd6944c5109037420867b006762a5e30dba
bpun1p <barry_pun@hotmail.com>
2023-02-01 21:03:41 -0800

    app clean-up unnecessary code from create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<<<<<<< HEAD
-=======
-        <p>
-        </p>
->>>>>>> f55b4e38135fddfc78643decb50ea668f188339e
-      </header>
+      
     </div>
   );
 }
