25c9bb2b1bd5ddf5422118312336af8b86e6ede3
bpun1p <barry_pun@hotmail.com>
2023-02-04 23:17:09 -0800

    remove unnecessary import

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import CurrentWeather from './components/current-weather/current-weather';
-import Forecast from './components/forecast/forecast';
 
 function App() {
   return (
