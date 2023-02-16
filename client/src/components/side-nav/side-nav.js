5052f4bd94b0c3f96559b4f5684a5592b207c58a
bpun1p <barry_pun@hotmail.com>
2023-02-16 01:25:55 -0800

    slide animation for side-nav implemented "

--- client/src/components/side-nav/side-nav.js
+++ client/src/components/side-nav/side-nav.js
@@ -1,11 +1,12 @@
 import React, { useState }  from 'react';
 import { Link } from 'react-router-dom';
+import './side-nav.css';
 
 export default function SideNav() {
   const [toggleOn, setToggleOn] = useState(false);
   const [navProps, setNavProps] = useState({
     text : 'Hide',
-    display : 'Block'
+    class : 'displayed'
   });
 
   const toggleNav = () => {
@@ -13,45 +14,45 @@ export default function SideNav() {
     if (!toggleOn) {
       setNavProps({...navProps,
         text : 'Show',
-        display : 'None'
+        class : 'dismissed'
       });
     } else {
         setNavProps({...navProps,
           text : 'Hide',
-          display : 'Block'
+          class : 'displayed'
         });
       }
   };
 
   return (
-    <nav className='side-nav'>
+    <nav className={`nav ${navProps.class}`}>
       <div className='nav-toggler'>
-        <button className='toggle-tbn' onClick={toggleNav} id='toggler'>{navProps.text}</button>
+        <button className='toggle-btn' onClick={toggleNav} id='toggler'>{navProps.text}</button>
       </div>
-      <div className='nav-content'style={{display: navProps.display}}>
+      <div className='side-nav'>
         <div className='dashboard'>
           <Link to='/dashboard'>
-            <button type='button' className='dashboard-btn'>Dashboard</button>
+            <button type='button' className='dashboard btn'>Dashboard</button>
           </Link>
         </div>
         <div className='search'>
           <Link to='/search'>
-            <button type='button' className='search-btn'>Search</button>
+            <button type='button' className='search btn'>Search</button>
           </Link>
         </div>
         <div className='library'>
           <Link to='library'>
-            <button type='button' className='library-btn'>Library</button>
+            <button type='button' className='library btn'>Library</button>
           </Link>
         </div>
         <div className='profile'>
           <Link to='profile'>
-            <button type='button'className='profile-btn'>Profile</button>
+            <button type='button'className='profile btn'>Profile</button>
           </Link>
         </div>
         <div className="learn-more">
           <Link to='Learn'>
-            <button type='button' className='learn-btn'>Learn</button>
+            <button type='button' className='learn btn'>Learn</button>
           </Link>
         </div>
       </div>
