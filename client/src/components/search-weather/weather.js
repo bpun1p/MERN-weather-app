2f36bc1ee5c99854c995187e327628f30f85b75d
bpun1p <barry_pun@hotmail.com>
2023-04-10 22:28:13 -0700

    changed input tag to form field, user able to search location

--- client/src/components/search-weather/weather.js
+++ client/src/components/search-weather/weather.js
@@ -9,135 +9,133 @@ import Sun from '../assets/images/sunny.png';
 import Clear from '../assets/images/clear.png'
 
 export default function CurrentWeather() {
-  const [data, setData] = useState({});
   const [forecastData, setForecastData] = useState({});
-  const [currentCondition, setCurrentCondition] = useState({});
-  const [location, setLocation] = useState('');
-  const [coordinates, setCoordinates] = useState({
-    latitude : '',
-    longitude : ''
+  const appid = process.env.REACT_APP_API_KEY
+  const [location, setLocation] = useState('')
+  const [data, setData] = useState({
+    location: '',
+    temp: '',
+    description: '',
+    feels_like: '',
+    humidity: '',
+    wind_speed: ''
   });
-
-  const defaultLocationUrl = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
-  const searchedWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
-  const searchedForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${process.env.REACT_APP_API_KEY}&cnt=5`;
-  const searchFromGeolocation = `http://api.openweathermap.org/geo/1.0/reverse?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${process.env.REACT_APP_API_KEY}`;
+  const geocodingUrl = new URL(`https://api.openweathermap.org/geo/1.0/reverse?`)
+  const weatherUrl = new URL(`https://api.openweathermap.org/data/2.5/weather?`)
+  const forecastUrl = new URL(`https://api.openweathermap.org/data/2.5/forecast?`)
 
   const weatherConditions = {
     "Snow": Snow,
     "Clouds": Clouds,
     "Rain": Rain,
     "Sun": Sun,
-    "Clear" : Clear
+    "Clear": Clear
   };
 
-  // useEffect(() => {
-  //   axios.get(defaultLocationUrl).then((res) => {
-  //     navigator.geolocation.getCurrentPosition(showPosition);
-  //     if (coordinates.latitude !== '') {
-  //       //fetch method to get from api
-  //     }
-  //     setData(res.data);
-  //     setCurrentCondition(res.data.weather[0].main);
-  //   }).then(() => {
-  //     getForcastData();
-  //   })    
-  // }, [])
-
-
   useEffect(() => {
-    if (coordinates.latitude && coordinates.longitude !== '') {
-      getLocalWeather();
+    if (data.location !== '') {
+      getWeatherData();
     } else {
       navigator.geolocation.getCurrentPosition(showPosition);
     }
-  },[coordinates])
+  },[data.location]);
 
-  const getLocalWeather = () => {
-    axios.get(searchFromGeolocation)
-    .then((res) => {
-      setLocation(res.data[0].name)
-    })
-      .then(() => {
-        axios.get(searchedWeatherUrl).then((res) => {
-          setData(res.data);
-          setCurrentCondition(res.data.weather[0].main);
+  const getWeatherData = () => {
+    if (data.location !== '') {
+      weatherUrl.search = new URLSearchParams({
+        q : data.location,
+        units : 'metric',
+        appid : appid
+      })
+      axios.get(weatherUrl)
+        .then((res) => {
+          setData({...data,
+            temp: res.data.main.temp.toFixed(),
+            description: res.data.weather[0].main,
+            feels_like: res.data.main.feels_like.toFixed(),
+            humidity: res.data.main.humidity,
+            wind_speed: res.data.wind.speed
+          })
         })
         .then(() => {
           getForcastData();
-        });
-    })
+        }) 
+    }
   }
   
-  const searchedLocation = (event) => {
-    setLocation(event.target.value)
-    if (event.key === 'Enter') {
-      axios.get(searchedWeatherUrl)
-      .then((res) => {
-        setData(res.data);
-        setCurrentCondition(res.data.weather[0].main);
-      }).then(() => {
-        getForcastData();
-      })
-      setLocation('');
-    }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setData({...data, location : location})
+    event.target.reset();
   };
 
   const showPosition = (position) => {
-    if (position.coords.latitude && position.coords.longitude) {
-      setCoordinates({...coordinates,
-        latitude : position.coords.latitude,
-        longitude : position.coords.longitude
+    if (position) {
+      geocodingUrl.search = new URLSearchParams({
+        lat : position.coords.latitude,
+        lon : position.coords.longitude,
+        appid : appid
       })
+      axios.get(geocodingUrl)
+        .then((res) => {
+          setData({...data, location: res.data[0].name})
+        })
     }
   }
 
   const getForcastData = () => {
-    let url = searchedForecastUrl
-    if (location === '') {
-      url = `https://api.openweathermap.org/data/2.5/forecast?q=vancouver&units=metric&appid=${process.env.REACT_APP_API_KEY}&cnt=5`
-    }
-    axios.get(url)
+    if (data.location !== '') {
+      forecastUrl.search = new URLSearchParams({
+        q : data.location,
+        cnt : '5',
+        units : 'metric',
+        appid : appid
+      })
+      axios.get(forecastUrl)
       .then((res) => {
         setForecastData(res.data);
       })
+    }
   }
 
   return (
     <div className='container'>
       <div className='search'>
-        <input 
-          value={location} 
-          onChange={searchedLocation}
-          onKeyDown={searchedLocation} 
-          type='text' 
-          placeholder='Enter Location'
-        />
+        <form onSubmit={onSubmit}>
+          <input
+              type="text"
+              placeholder="Enter location"
+              onChange={ e =>{
+                setLocation(e.target.value);
+              }}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
       <div>
         <div className='top'>
           <div className='location'>
-            <h1>{data.name}</h1> 
+            {data.location !== '' ? <h1>{data.location}</h1> : null}
           </div>
           <div className='temp'>
-            {data.main ? <p>{data.main.temp.toFixed()}°C</p> : null}
-            <img src={weatherConditions[currentCondition]} className='weather-img' alt='weather-img' />
+            {data.temp !== '' ? <p>{data.temp}°C</p> : null}
+            <img src={weatherConditions[data.description]} className='weather-img' alt='weather-img' />
           </div>
           <div className='description'>
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.description !== '' ? <p>{data.description}</p> : null}
           </div>
         </div>
         <div className='center'>
           <div className='feels'>
-            {data.main ? <p>{data.main.feels_like.toFixed()}°C</p> : null}
+            {data.feels_like !== '' ? <p>{data.feels_like}°C</p> : null}
             <p>Feels like</p>
           </div>
           <div className='humidity'>
-            {data.main ? <p>{data.main.humidity}%</p> : null}
+            {data.humidity !== '' ? <p>{data.humidity}%</p> : null}
             <p>Humidity</p>
           </div>
           <div className='winds'>
-            {data.wind ? <p>{data.wind.speed} MPH</p> : null}
+            {data.wind_speed !== '' ? <p>{data.wind_speed} MPH</p> : null}
             <p>Winds</p>
           </div>
         </div>
