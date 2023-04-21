d4ff5ea5f8864b15dfd5268c26d47fa9f87063d8
bpun1p <barry_pun@hotmail.com>
2023-04-21 00:06:25 -0700

    Api routes for search and library params added and routed

--- server/server.js
+++ server/server.js
@@ -1,27 +1,18 @@
 const express = require('express');
+const PORT = 3000;
 const mongoose = require('mongoose');
 const Location = require('./models/location');
+const locationRoutes = require('./routes/locationRoutes');
 
 //express app
 const app = express();
 
 //connect to mongodb
 const dbURI = 'mongodb+srv://bpun1p:Test-1234@weatherdb.upjrnjk.mongodb.net/weatherDB?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => app.listen(3000))
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`)))
   .catch((err) => console.log(err));
-
-app.get('/add-location', (req, res) => {
-  const location = new Location({
-    location: 'Vancouver'
-  });
-  location.save()
-    .then((result) => {res.send(result)})
-    .catch((err) => {console.log(err)});
-})
-
-app.get('/all-locations', (req, res) => {
-  Location.find()
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err))
-})
+  
+//Api routes  
+app.use('/search', locationRoutes);
+app.use('/library', locationRoutes);
