81ea40323404db0bf53e7979eeea61ac13a2d6e9
bpun1p <barry_pun@hotmail.com>
2023-04-22 00:06:01 -0700

    get, post, delete services for location implmented

---
+++ client/src/service/libraryService.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const currentUrl = window.location.href;
+
+export const getLocations = () => {
+  axios.get(`${currentUrl}/library`)
+    .then((res) => {res.json().then((data) => data)})
+    .catch(err => console.error(err));
+};
+
+export const deleteLocation = (id) => {
+  axios.delete(`${currentUrl}/library`, { id: id })
+    .then(() => {
+      return { message : { msgBody : 'Location deleted' }, msgError : false};
+    })
+    .catch((err) => {
+      return { message : { msgBody : err }, msgError : true };
+    })
+};
+
+export const saveLocation = (location) => {
+  axios.post(`${currentUrl}/search`, location)
+    .then(() => {
+      return { message : { msgBody : 'Location saved' }, msgError : false};
+    })
+    .catch((err) => { 
+      return { message : { msgBody : err }, msgError : true };
+  });
+};
