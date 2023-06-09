3a5a850510fc87cc72840548a7241727edd6ab0f
bpun1p <barry_pun@hotmail.com>
2023-06-08 17:37:59 -0700

    context provider created and wrapped around app"

--- client/src/components/Weather/Weather.js
+++ client/src/components/Weather/Weather.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState, useCallback} from 'react';
 import './Weather.css';
 import Forecast from '../forecast/Forecast';
-import { weatherConditions } from '../utils/WeatherConditions/WeatherConditions';
+import { weatherConditions } from '../utils/weatherConditions/WeatherConditions';
 import { saveLocation } from '../../service/libraryService';
 import { getCurrent, getForecast, geocodingService } from '../../service/weatherService';
 
