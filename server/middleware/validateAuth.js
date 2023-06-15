fdb51067d60d8099ece4751e76b9e9b277b8f1a8
bpun1p <barry_pun@hotmail.com>
2023-06-14 17:08:49 -0700

    middleware to require authorized token created and setup in the locations route

---
+++ server/middleware/validateAuth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const validateAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({error: 'require authorization token'});
+  };
+
+  const token = authorization.split(' ')[1];
+
+  try {
+    const {_id} = jwt.verify(token, process.env.SECRET);
+    req.id = await User.findOne({_id}).select('_id');
+    next();
+  }
+  catch(err) {
+    console.log(error);
+    res.status(401).json({error: 'requiest is not authorized'})
+  }
+}
+
+module.exports = validateAuth;
