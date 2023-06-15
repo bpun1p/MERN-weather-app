2d1f2e76e06a31d9dbf04394186bf9882122d9e4
bpun1p <barry_pun@hotmail.com>
2023-06-15 13:55:04 -0700

    create loading spinner component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Profile.css'
 
 export default function Profile() {
+  
   return (
     <div className='profile'>
       <h1 className='profile-heading'>Coming Soon</h1>
