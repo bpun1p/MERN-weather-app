2f939848faf005c31201567aff7657b437c1ea93
bpun1p <barry_pun@hotmail.com>
2023-07-18 13:25:14 -0700

    change currentURL for prod; and disable react dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
